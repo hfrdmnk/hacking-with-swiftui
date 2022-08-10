8f44797faeff0b1bff188db61c3165c9c9e6ddfa
Dominik Hofer <hfrdmnk@gmail.com>
2022-08-10 21:06:42 +0200

    started CupcakeCorner project

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Dominik Hofer on 10.08.22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
