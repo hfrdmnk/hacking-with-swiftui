c987f8eab3a2df20e884cd6a984f726e5109882e
Dominik Hofer <hfrdmnk@gmail.com>
2022-08-12 20:47:19 +0200

    CupcakeCorner part II

---
+++ CupcakeCorner/CupcakeCorner/AddressView.swift
@@ -0,0 +1,43 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Dominik Hofer on 11.08.22.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    @ObservedObject var order: Order
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street address", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+            
+            Section {
+                NavigationLink {
+                    CheckoutView(order: order)
+                } label: {
+                    Text("Check out")
+                }
+            }
+            .disabled(!order.hasValidAddress)
+        }
+        .navigationTitle("Delivery details")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AddressView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            AddressView(order: Order())
+        }
+        .preferredColorScheme(.dark)
+    }
+}
