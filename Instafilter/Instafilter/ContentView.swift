c2d7b194ee40de9b5a91cbde44b4993d484d804e
Dominik Hofer <hfrdmnk@gmail.com>
2022-08-26 20:44:56 +0200

    finished intro to Instafilter project

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -8,23 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var image: Image?
-    @State private var showingImagePicker = false
-    
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-            
-            Button("Select Image") {
-                showingImagePicker = true
-            }
-            .buttonStyle(.bordered)
-        }
-        .sheet(isPresented: $showingImagePicker) {
-            ImagePicker()
-        }
+        Text("Instafilter")
+            .padding()
     }
 }
 
---
+++ Instafilter/Instafilter/ImageSaver.swift
@@ -0,0 +1,18 @@
+//
+//  ImageSaver.swift
+//  Instafilter
+//
+//  Created by Dominik Hofer on 26.08.22.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
+    }
+    
+    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        print("Save finished!")
+    }
+}
