e14ebfbf8b856ee28b3500319924e3d9fa745c99
Dominik Hofer <hfrdmnk@gmail.com>
2022-09-02 15:12:53 +0200

    part II of intro for Bucketlist project

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -5,12 +5,40 @@
 //  Created by Dominik Hofer on 31.08.22.
 //
 
+import LocalAuthentication
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isUnlocked = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your data."
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                if success {
+                    isUnlocked = true
+                } else {
+                    
+                }
+            }
+        } else {
+            
+        }
     }
 }
 
