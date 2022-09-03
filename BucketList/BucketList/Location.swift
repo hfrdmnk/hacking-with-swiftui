cef385655091f78b4b47abb7524b7780f5b324cd
Dominik Hofer <hfrdmnk@gmail.com>
2022-09-03 17:41:18 +0200

    started with BucketList project

---
+++ BucketList/BucketList/Location.swift
@@ -0,0 +1,27 @@
+//
+//  Location.swift
+//  BucketList
+//
+//  Created by Dominik Hofer on 03.09.22.
+//
+
+import Foundation
+import CoreLocation
+
+struct Location: Identifiable, Codable, Equatable {
+    var id: UUID
+    var name: String
+    var description: String
+    let latitude: Double
+    let longitude: Double
+    
+    var coordinate : CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    static let example = Location(id: UUID(), name: "Buckingham Palace", description: "Where Queen Elizabeth II. lives with her dorgis", latitude: 51.501, longitude: -0.141)
+    
+    static func ==(lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
+}
