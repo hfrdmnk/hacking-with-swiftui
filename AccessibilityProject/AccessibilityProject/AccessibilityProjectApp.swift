8c3812563ebdf5b543aa57cea9d24ddef0493fb3
Dominik Hofer <hfrdmnk@gmail.com>
2022-09-07 21:57:01 +0200

    startet accessiblity project

---
+++ AccessibilityProject/AccessibilityProject/AccessibilityProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityProjectApp.swift
+//  AccessibilityProject
+//
+//  Created by Dominik Hofer on 07.09.22.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
