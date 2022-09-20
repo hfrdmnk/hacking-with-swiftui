b06e946fc92a69a2bda3b18a4e2379caa513cd92
Dominik Hofer <hfrdmnk@gmail.com>
2022-09-20 08:18:44 +0200

    HotProspects day II

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Dominik Hofer on 19.09.22.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
