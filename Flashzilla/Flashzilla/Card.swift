33560e40bc8a991723a4748d64d64a3da337e31b
Dominik Hofer <hfrdmnk@gmail.com>
2022-10-01 21:09:25 +0200

    first coding day on flashzilla

---
+++ Flashzilla/Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Dominik Hofer on 01.10.22.
+//
+
+import Foundation
+
+struct Card {
+    let prompt: String
+    let answer: String
+    
+    static let example = Card(prompt: "Who is the boss of Dunder Mifflin Scranton?", answer: "Michael Scott")
+}
--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -7,10 +7,39 @@
 
 import SwiftUI
 
-struct ContentView: View {    
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(x: 0, y: offset * 10)
+    }
+}
+
+struct ContentView: View {
+    @State private var cards = Array<Card>(repeating: Card.example, count: 10)
+    
     var body: some View {
-        Text("Hello World!")
-            .padding()
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index]) {
+                            withAnimation {
+                                removeCard(at: index)
+                            }
+                        }
+                            .stacked(at: index, in: cards.count)
+                    }
+                }
+            }
+        }
+    }
+    
+    func removeCard(at index: Int) {
+        cards.remove(at: index)
     }
 }
 
