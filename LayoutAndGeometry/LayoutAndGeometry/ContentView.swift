1b5391612c8f2cb0b947dc2ff8fcea5016b587ef
Dominik Hofer <hfrdmnk@gmail.com>
2022-10-15 22:48:13 +0200

    GeometryReader challenge done

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -8,18 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    let colors: [Color] = [.blue, .indigo, .pink, .purple, .mint]
-    
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
     var body: some View {
         GeometryReader { fullView in
-            ScrollView {
+            ScrollView(.vertical) {
                 ForEach(0..<50) { index in
                     GeometryReader { geo in
                         Text("Row #\(index)")
                             .font(.title)
                             .frame(maxWidth: .infinity)
-                            .background(colors[index % 5])
+                            .background(Color(hue: Double(geo.frame(in: .global).minY.interpolated(from: 0...fullView.size.height, to: 0...1)), saturation: 1, brightness: 1))
                             .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .opacity(Double(geo.frame(in: .global).minY).interpolated(from: 0...200, to: 0...1))
+                            .scaleEffect(Double(geo.frame(in: .global).minY.interpolated(from: 0...fullView.size.height, to: 0.5...1)))
                     }
                     .frame(height: 40)
                 }
@@ -32,4 +34,25 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+extension FloatingPoint {
+  /// Allows mapping between reverse ranges, which are illegal to construct (e.g. `10..<0`).
+  func interpolated(
+    fromLowerBound: Self,
+    fromUpperBound: Self,
+    toLowerBound: Self,
+    toUpperBound: Self) -> Self
+  {
+    let positionInRange = (self - fromLowerBound) / (fromUpperBound - fromLowerBound)
+    return (positionInRange * (toUpperBound - toLowerBound)) + toLowerBound
+  }
+
+  func interpolated(from: ClosedRange<Self>, to: ClosedRange<Self>) -> Self {
+    interpolated(
+      fromLowerBound: from.lowerBound,
+      fromUpperBound: from.upperBound,
+      toLowerBound: to.lowerBound,
+      toUpperBound: to.upperBound)
+  }
 }
